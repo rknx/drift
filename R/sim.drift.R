ec08c1c39c783bc4a1c6f8948a521a5d9ac9f6de
अनुज शर्मा <rknx@outlook.com>
2018-01-31 21:45:36 -0500

    Fix plot error

--- R/sim.drift.R
+++ R/sim.drift.R
@@ -4,12 +4,12 @@
 #' @param n.pop Number of effective population
 #' @param n Ploidy: Haploid=1, Diploid=2...
 #' @param freq initial frequency of allele (0-1)
-#' @param n.gen Number of generations to simulate
+#' @param n.gen Number of generations to simulate Default:100
 #' @param n.gene Number of genes/loci tested
 #' @param n.time Number of simulations to perform
-#' @param s.plot Save the image of plot? (TRUE/FALSE)
-#' @param s,table Save the table of numerical valuest? (TRUE/FALSE)
-#' @return Plots and Graphs are saved to working directory
+#' @param s.plot Save the image of plot? (TRUE/FALSE) Default:FALSE
+#' @param s,table Save the table of numerical valuest? (TRUE/FALSE) Default:FALSE
+#' @return Frequency Plots. If save is true, image and tabel text file are saved in working dierctory.
 #' @usage sim.drift(n.pop,n,freq,n.gen,n.gene,n.time,s.plot,s.table)
 #' @name sim.drift
 #' @export
@@ -24,7 +24,7 @@ library(ggplot2)                        # For plotting graph
 library(reshape2)                        # For refrshing the plot for new simulations
 
 # Monte-carlo function
-sim.drift <- function (n.pop, n, freq, n.gen, n.gene, n.time, s.plot, s.table) {
+sim.drift <- function (n.pop, n=1, freq, n.gen=100, n.gene, n.time, s.plot=FALSE, s.table=FALSE) {
   n.pop <- n.pop * n									# Adjust allele count for diploid
   timestamp <- floor(as.numeric(Sys.time()))			# Unique timestmamp for the files
   
@@ -40,12 +40,12 @@ sim.drift <- function (n.pop, n, freq, n.gen, n.gene, n.time, s.plot, s.table) {
     
     # Change table format and plot it
     X <- data.frame(X/n.pop)							# Normalize the dataframe
-    ggplot(melt(X), aes(x = rep(c(1:n.gen), n.gene), y = value, colour = variable)) + geom_line() + labs(title = "Simulations of Genetic Drift", x="Generation", y="Allele Frequency") + ylim(0,1) + theme(legend.position = "none")
+    colnames(X) = paste0("Line",1:n.gene)
+    print(ggplot(melt(X), aes(x = rep(c(1:n.gen), n.gene), y = value, colour = variable)) + geom_line() + labs(title = "Simulations of Genetic Drift", x="Generation", y="Allele Frequency") + ylim(0,1) + theme(legend.position = "none"))
 														# Plotting
     # Saving the files
-    colnames(X) = paste0("Line",1:n.gene)
-    if (s.table == TRUE) write.table(X, file=paste0("Genetic Drift Simulation ",timestamp,"-",k,".txt"), sep="\t")   # Optional, save CSV
-    if (s.plot == TRUE) ggsave(paste0("Genetic Drift Simulation ",timestamp,"-",k,".png"), type="cairo-png")        # Optional, save graph
+    if (s.table == TRUE) {write.table(round(X,digits=3), file=paste0("Genetic Drift Simulation ",timestamp,"-",k,".txt"), sep="\t")}   # Optional, save CSV
+    if (s.plot == TRUE) {ggsave(paste0("Genetic Drift Simulation ",timestamp,"-",k,".png"), type="cairo-png")}        # Optional, save graph
   }
   print(paste0("The timestamp is: ",timestamp))
 }
